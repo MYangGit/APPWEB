6ea21be0eefb7c5bd1506744171639d0c1902727
xuhuanyu <xuhuanyu@tongyuan.cc>
2024-03-21 11:32:17 +0800

    update

--- src/components/DataSet.vue
+++ src/components/DataSet.vue
@@ -8,7 +8,11 @@ import { rootStore } from '@/stores/rootStore'
 
 const extensions = [json(), noctisLilac]
 
+const hasError = ref(false)
+
 const editor = ref()
+
+const tmpState = ref(rootStore.dataConfig.stateSet)
 
 const code = computed({
   get() {
@@ -18,15 +22,19 @@ const code = computed({
     let state = ''
     try {
       state = JSON.parse(value)
+      hasError.value = false
     } catch (error) {
-      console.error('json格式异常，当前变更无法同步')
+      hasError.value = true
     }
-    if (state) {
-      rootStore.dataConfig.stateSet = state
-      console.log('已同步')
+    if (!hasError.value) {
+      tmpState.value = state
     }
   }
 })
+
+const handleBlur = () => {
+  rootStore.dataConfig.stateSet = tmpState.value
+}
 
 </script>
 
@@ -36,14 +44,33 @@ const code = computed({
       ref="editor"
       v-model="code"
       :autofocus="false"
+      @blur="handleBlur"
       :indent-with-tab="true"
       :tab-size="2"
       :extensions="extensions"
     />
+    <div class="status-bar" :class="hasError ? 'red': 'green' ">{{ hasError ? '数据格式错误' : '正常' }}</div>
   </div>
 </template>
 <style lang="less" scoped>
 .data-set-wrapper {
   height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.status-bar {
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  padding: 3px 0 3px 10px;
+  &.green {
+    color: #ffffff;
+    background-color: #67C23A;
+  }
+  &.red {
+    color: #ffffff;
+    background-color: #F56C6C;
+  }
 }
 </style>
