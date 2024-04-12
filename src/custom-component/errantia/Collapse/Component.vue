44ca6149ada9275c5697b4436bf55b02172d29bf
MYangGit <93202929+MYangGit@users.noreply.github.com>
2024-04-12 11:26:03 +0800

    feat：添加折叠面板组件

---
+++ src/custom-component/errantia/Collapse/Component.vue
@@ -0,0 +1,134 @@
+<!-- eslint-disable vue/no-v-html -->
+<template>
+    <div>
+        <div class="collapse-content">
+            <el-collapse
+                v-model="propValue.activePanelName"
+                :accordion="propValue.accordion"
+                :class="{arrowPosition: propValue.arrowPosition === 'left', arrowNear: propValue.arrowNear}"
+                @change="handleChange"
+            >
+                <el-collapse-item
+                    v-for="item in propValue.panelLists" 
+                    :key="item.name"
+                    :title="item.title"  
+                    :name="item.name"
+                >
+                    <div :style="{width: '100%', height: item.height + 'px'}">
+                        <div v-if="editMode == 'edit'" style="width: 100%; height: 100%;" class="v-tabs">
+                            <Container
+                                :element="element"
+                                :name="element.items.filter((i) => i.label === `ErCollapse${item.name}`)[0].name"
+                                :childs="childs.filter((i) => i.activeName === element.items.filter((i) => i.label === `ErCollapse${item.name}`)[0].name)"
+                            />
+                        </div>
+                        <div v-else style="width: 100%; height: 100%;" class="v-tabs preview">
+                            <PreviewContainer
+                                :element="element"
+                                :name="element.items.filter((i) => i.label === `ErCollapse${item.name}`)[0].name"
+                                :childs="childs.filter((i) => i.activeName === element.items.filter((i) => i.label === `ErCollapse${item.name}`)[0].name)"
+                            />
+                        </div>
+                    </div>
+                </el-collapse-item>
+            </el-collapse>
+            <div style="flex-grow: 1;">
+                <div v-if="editMode == 'edit'" style="width: 100%; height: 100%;" class="v-tabs">
+                    <Container
+                        :element="element"
+                        :name="element.items.filter((i) => i.label === `ErCollapseonly`)[0]?.name"
+                        :childs="childs.filter((i) => i.activeName === element.items.filter((i) => i.label === `ErCollapseonly`)[0]?.name)"
+                    />
+                </div>
+                <div v-else style="width: 100%; height: 100%;" class="v-tabs preview">
+                    <PreviewContainer
+                        :element="element"
+                        :name="element.items.filter((i) => i.label === `ErCollapseonly`)[0]?.name"
+                        :childs="childs.filter((i) => i.activeName === element.items.filter((i) => i.label === `ErCollapseonly`)[0]?.name)"
+                    />
+                </div>
+            </div>
+        </div>
+    </div> 
+</template>
+
+<script>
+import Container from '../../common/Container.vue';
+import PreviewContainer from '../../common/PreviewContainer.vue';
+import OnEvent from '../../common/OnEvent';
+import { rootStore } from '@/stores/rootStore';
+import { useEventCentre } from '@/hooks/useEventCentre';   
+
+const { onChange } = useEventCentre();
+export default {
+    components: {
+        Container,
+        PreviewContainer,
+    },
+    extends: OnEvent,
+    props: {
+        propValue: {
+            type: Object,
+            default: () => ({
+                accordion: true,
+                arrowPosition: 'right',
+                arrowNear: false,
+                activePanelName: ['1'],
+                panelLists: [
+                    {
+                        name: '1',
+                        title: '面板1',
+                        height: 100,
+                    }
+                ],
+            }),
+        },
+        element: {
+            type: Object,
+            default: () => {},
+        }
+    },
+    data() {
+        return {
+        };
+    },
+    computed: {
+        editMode () {
+            return rootStore.editor.editMode
+        },
+        childs() {
+            return rootStore.dataCenter.componentData.filter((i) => i.pid === this.element.id);
+        },
+    },
+    methods: {
+        handleChange(val) {
+            onChange({element: this.element, newValue: val})
+        }
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.preview {
+    user-select: none;
+}
+.collapse-content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+}
+.arrowPosition {
+    :deep(.el-collapse-item__arrow) {
+        order: -1;
+        margin: 0;
+        margin-right: 5px;
+    }
+}
+.arrowNear {
+    :deep(.el-collapse-item__arrow) {
+        margin: 0;
+        margin-left: 5px;
+    }
+}
+</style>
