3d851d73813d2b289c14387daae3f55f88f1a484
MYangGit <93202929+MYangGit@users.noreply.github.com>
2024-05-11 18:04:15 +0800

    feat: 增加syslab通信函数

--- src/hooks/useGlobalUtils.js
+++ src/hooks/useGlobalUtils.js
@@ -1,14 +1,26 @@
-import { post, postMessage, getFilePath, initFilePath } from '@/services/request/connectBase'
-
+import { 
+  post, 
+  postMessage, 
+  getFilePath, 
+  initFilePath, 
+  checkValHas,
+  importDesignFile,
+  exportDesignFile
+} from '@/services/request/connectBase'
+import { returnData } from '@/constant'
 /**
  * @description 全局公共方法暴露中心
  * @returns 
 */
 export const useGlobalUtils = () => {
   return {
+    returnData,
+    importDesignFile,
+    exportDesignFile,
     post,
     postMessage,
     getFilePath,
-    initFilePath
+    initFilePath,
+    checkValHas
   }
 }
--- src/services/request/connectBase.js
+++ src/services/request/connectBase.js
@@ -34,7 +34,7 @@ export const postMessage = (config) => {
 // 处理消息返回
 window.addEventListener('message', event => {
   const message = event.data;
-  console.log('message', message)
+  // console.log('message', message)
   if (message.type === 'receiveData') {
     if (!message.result) {
       deferred.resolve(false)
@@ -63,4 +63,54 @@ export const initFilePath = async () => {
 
 export const getFilePath = () => {
   return filePath
+}
+
+// 检查变量是否存在
+export const checkValHas = async (varNames) => {
+  let message = await post({
+    key: 'checkVar',
+    command: 'getWorkspaceVariables',
+    filePath: getFilePath(),
+  })
+  if (message && message.data && message.type === 'receiveWorkspaceVariables') {
+    let varList = message.data.value.map((item) => item.fullname)
+    let catchVar;
+    for (let varname of varNames) {
+      if (varList.indexOf(varname) > -1) {
+        catchVar = varname
+        break;
+      }
+    }
+    return catchVar
+  }
+}
+
+// 导出设计文件
+export const importDesignFile = async () => {
+  let message = await post({
+    key: 'importTextFile',
+    command: 'importTextFile',
+    filePath: getFilePath()
+  })
+  let fileContent = message.data.value.slice(0, message.data.value.lastIndexOf('}\n') + 1);
+  let md5hash = message.data.value.slice(message.data.value.lastIndexOf('}\n') + 2);
+  if ((window).md5(fileContent) !== md5hash) {
+    // todo：提示文件被篡改 使用弹窗提示
+    return
+  }
+  return JSON.parse(fileContent);
+}
+
+// 加载 syslab 文件 param 必须满足JSON格式
+export const exportDesignFile = (param) => {
+  let calcAppConfig = getCalcAppConfig()
+  calcAppConfig.core = appConfig.core
+  let text = JSON.stringify(param, null, "\t");
+  // 执行导出获取返回
+  post({
+    key: 'exportTextFile',
+    command: 'exportTextFile',
+    content: `${text}\n${(window).md5(text)}`,
+    filePath: getFilePath(),
+  })
 }
