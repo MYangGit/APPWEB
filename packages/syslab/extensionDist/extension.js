1bfee25ebe9e62fc107d3d506888737a56b08e02
MYangGit <93202929+MYangGit@users.noreply.github.com>
2024-09-10 15:30:05 +0800

    feat: 添加天线阵列设计器app

--- packages/syslab/extensionDist/extension.js
+++ packages/syslab/extensionDist/extension.js
@@ -127,7 +127,7 @@ module.exports = require("vscode");
 /* 5 */
 /***/ ((module) => {
 
-module.exports = /*#__PURE__*/JSON.parse('{"appName":"antennaDesigner","displayName":"antennaDesigner","startCommand":"antennaDesigner","startTitle":"antennaDesigner","publishMoHub":false,"MoHubPort":47736,"icon":"app-icon.png","version":"1.0.0","description":"这是一个天线设计器","appTitle":"天线设计器","appTitleEn":"antennaDesigner","appHeight":750,"appWidth":1200,"appType":"julia"}');
+module.exports = /*#__PURE__*/JSON.parse('{"appName":"antennaArrayDesigner","displayName":"antennaArrayDesigner","startCommand":"antennaArrayDesigner","startTitle":"antennaArrayDesigner","publishMoHub":false,"MoHubPort":47736,"icon":"app-icon.png","version":"1.0.0","description":"这是一个天线阵列设计器","appTitle":"天线阵列设计器","appTitleEn":"antennaArrayDesigner","appHeight":750,"appWidth":1200,"appType":"julia"}');
 
 /***/ }),
 /* 6 */
