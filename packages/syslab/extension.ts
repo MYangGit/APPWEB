96e9b2a918675592085c0576bf34e38b880f3102
MYangGit <93202929+MYangGit@users.noreply.github.com>
2025-07-21 14:53:17 +0800

    chore：更新syslab打包基层

--- packages/syslab/extension.ts
+++ packages/syslab/extension.ts
@@ -4,31 +4,50 @@ import * as cp from 'child_process';
 import * as vscode from 'vscode';
 import appConfig from './extension-build.json';
 
-function getWebViewContent(context: vscode.ExtensionContext, templatePath: string, urlPath: string): string {
-  const resourcePath = path.join(context.extensionPath, templatePath);
+const getQueryVariable = (variable, location) => {
+  const query = location.search.substring(1);
+  const vars = query.split('&');
+  for (let i = 0; i < vars.length; i++) {
+    const pair = vars[i].split('=');
+    if (pair[0] === variable) { return pair[1]; }
+  }
+  return (false);
+};
+
+async function getWebViewContent(context, templatePath, urlPath) {
+  let location = await vscode.commands.executeCommand('Syslab.getWindowLocationInfo') as any;
+  const syslabPort = getQueryVariable('syslabPort', location);
+	const syslabIp = getQueryVariable('syslabIp', location);
+  let pathtemp = '';
+  if (syslabPort && syslabIp) {
+    pathtemp = `/stable/${syslabPort}/${syslabIp}`;
+  } else if (syslabPort && !syslabIp) {
+    pathtemp = `/stable/${syslabPort}`;
+  }
+  pathtemp = location.pathname + pathtemp;
+	pathtemp = pathtemp === '/' ? '' : pathtemp;
+  if (location && location.href) {
+    urlPath = pathtemp
+  }
+  const resourcePath = path.join(context.extensionPath, templatePath)
   let html = fs.readFileSync(resourcePath, 'utf-8');
-  
-  let port = appConfig.MoHubPort;
   html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src="|url\(")(.+?)"/g, (m, $1, $2) => {
-    if(appConfig.publishMoHub){
-      return $1 + (urlPath || '') + `/cn-north-4/syslabonline//stable/${port}/vscode-remote-resource?path=` + context.extensionPath + '/dist' + $2 + '"';
-    }
-    return $1 + (urlPath || '') + '/vscode-remote-resource?path=' + context.extensionPath + '/dist' + $2 + '"';
-  });
-
-  return html;
+    let pre = $1 + (urlPath || '')
+    if(pre[pre.length - 1] !== '/') pre += '/'
+    return  pre + 'vscode-remote-resource?path=' + context.extensionPath + '/dist' + $2 + '"'
+  })
+  return html
 }
-
 
 function activate(context: vscode.ExtensionContext) {
   let startAppCommand = appConfig.startCommand ?? 'test-org.startTestApp';
-  let disposable = vscode.commands.registerCommand(startAppCommand, (urlPath: string) => {
+  let disposable = vscode.commands.registerCommand(startAppCommand, async (urlPath: string) => {
     vscode.commands.executeCommand('start app', {
       id: appConfig.appName,
       title: appConfig.appTitle ?? 'TestApp',
       titleEn: appConfig.appTitleEn ?? 'TestApp',
-      html: getWebViewContent(context, './dist/index.html', urlPath),
-      filePath: process.env.USER_DATA_DIR || '/home/tongyuan/SyslabCloud/code-server',
+      html: await getWebViewContent(context, './dist/index.html', urlPath),
+      filePath: process.env.TONGYUAN_PATH || '/home/tongyuan/SyslabCloud/code-server',
       // filePath: 'C:/Users/admin/syslabCloud',
       width: appConfig.appWidth ?? 1080,
       height: appConfig.appHeight ?? 750,
